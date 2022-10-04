c39d310aadea61475c31be47dfc5682e7873a98f
star-dark <den1116@naver.com>
2022-10-04 09:00:04 +0900

    structure

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type info struct {
+	name string
+	age  int
+}
+
+func (i info) sayHello() {
+	fmt.Printf("내 이름은 %s 내 나이는 %d야", i.name, i.age)
+}
+
 func main() {
-	fmt.Println("It's Work")
+	introduce := info{"대형", 23}
+	introduce.sayHello()
 }
