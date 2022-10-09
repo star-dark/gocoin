a0fd59905bee19446a53d1e2f0389c84262d1d07
star-dark <den1116@naver.com>
2022-10-09 19:36:00 +0900

    .

--- main.go
+++ main.go
@@ -1,40 +1,5 @@
 package main
-
-import (
-	"crypto/sha256"
-	"fmt"
-)
-
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
-type blockchain struct {
-	blocks []block
-}
-
-func (b *blockchain) getLastHash() string {
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-	return ""
-}
-
-func (b *blockchain) addBlock(data string) {
-	newBlock := block{data, "", b.getLastHash()}
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.hash))
-	newBlock.hash = fmt.Sprintf("%x", hash)
-	b.blocks = append(b.blocks, newBlock)
-}
-
-func (b *blockchain) listBlock() {
-	for _, block := range b.blocks {
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("Hash: %s\n", block.hash)
-		fmt.Printf("PrevHash: %s\n", block.prevHash)
-	}
-}
+import 
 
 func main() {
 	chain := blockchain{}
