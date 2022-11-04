3b2b0bd397c80d9ef3d11887fa9e96a205a5841a
star-dark <den1116@naver.com>
2022-11-04 10:44:57 +0900

    partial

--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"text/template"
 
 	"github.com/star-dark/gocoin/blockchain"
 )
 
+var templates *template.Template
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlock()}
+	templates.ExecuteTemplate(rw, "home", data)
+}
+func add(rw http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(rw, "add", nil)
+}
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("second Block")
-	chain.AddBlock("third Block")
-	chain.AddBlock("Fourth Block")
-	for _, block := range chain.AllBlock() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("PrevHash: %s\n", block.PrevHash)
-	}
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partial/*.gohtml"))
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(port, nil))
+	fmt.Printf("Listening on http://localhost%s\n", port)
 }
